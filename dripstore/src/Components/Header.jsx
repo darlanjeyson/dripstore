a1c944019a0cb2bb118fc4a3d087b756add9a61f
DARLAN JEYSON MACEDO CORREIA <darlancorreia0711@digitalcollege.internal>
2025-10-22 22:05:14 -0300

    first commit

---
+++ dripstore/src/Components/Header.jsx
@@ -0,0 +1,31 @@
+import "../App.css"
+import Logo from "../assets/Logo.png"
+
+function Header() {
+
+  return (
+    <>
+     <section id="header-container">
+        <div id="logo-image">
+            <img src={Logo} alt="" />
+            
+        </div>
+
+        <div id="input-container">
+            <input type="text" name="" id="" placeholder="Pesquisar produtos..."/>
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+
+        <div id="cart-container">
+            <div id="cart-redirections">
+                <a href="">Cadastre-se</a>
+                <button>Entrar</button>
+            </div>
+            <i class="fa-solid fa-cart-shopping"></i>
+        </div>
+     </section>
+    </>
+  )
+}
+
+export default Header
